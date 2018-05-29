ab8e042f889c7082dba1f529ec4ba5d32bb37f96
Ivan Andonov <ivan.andonovv@hotmail.com>
2018-05-29 18:00:37 +0200

    shooting projectiles at enemies

--- TowerDefense/Assets/Scripts/Tower/Tower.cs
+++ TowerDefense/Assets/Scripts/Tower/Tower.cs
@@ -10,18 +10,96 @@ public class Tower : MonoBehaviour {
 	[SerializeField]
 	private float attackRadius;
 
+	[SerializeField]
 	private Projectile projectile;
+
 	private Enemy targetEnemy = null;
 	private float attackCounter;
+	private bool isAttacking = false;
 
 	// Use this for initialization
-	void Start () {
-		
+	void Start() {
+
 	}
-	
+
 	// Update is called once per frame
-	void Update () {
-		
+	void Update() {
+		attackCounter -= Time.deltaTime;
+		if (targetEnemy == null)
+		{
+			Enemy nearestEnemy = GerNearestEnemyInRange();
+			if (nearestEnemy != null && Vector2.Distance(transform.localPosition, nearestEnemy.transform.localPosition) <= attackRadius)
+			{
+				targetEnemy = nearestEnemy;
+			}
+		}
+		else
+		{
+			if(attackCounter <= 0)
+			{
+				isAttacking = true;
+				attackCounter = timeBetweenAttacks; // reset attack counter
+			}
+			else
+			{
+				isAttacking = false;
+			}
+			if (Vector2.Distance(transform.localPosition, targetEnemy.transform.localPosition) > attackRadius)
+			{
+				targetEnemy = null;
+			}
+		}
+	}
+
+	private void FixedUpdate()
+	{
+		if (isAttacking)
+			Attack();
+	}
+
+	public void Attack()
+	{
+		isAttacking = false;
+		Projectile newProjectile = Instantiate(projectile) as Projectile;
+		newProjectile.transform.localPosition = transform.localPosition;
+		if (targetEnemy == null)
+		{
+			Destroy(newProjectile);
+		}
+		else
+		{
+			// move projectile to enemy
+			StartCoroutine(MoveProjectile(newProjectile));
+		}
+	}
+
+	IEnumerator MoveProjectile(Projectile projectile)
+	{
+		while (GetTargetDistance(targetEnemy) > 0.20f && projectile != null && targetEnemy != null)
+		{
+			var direction = targetEnemy.transform.localPosition - transform.localPosition;
+			var angleDirection = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+			projectile.transform.rotation = Quaternion.AngleAxis(angleDirection, Vector3.forward);
+			projectile.transform.localPosition = Vector2.MoveTowards(projectile.transform.localPosition, targetEnemy.transform.localPosition, 5f * Time.deltaTime);
+			yield return null;
+		}
+		if (projectile != null || targetEnemy == null)
+		{
+			Destroy(projectile);
+		}
+	}
+
+	private float GetTargetDistance(Enemy thisEnemy)
+	{
+		if(thisEnemy == null)
+		{
+			thisEnemy = GerNearestEnemyInRange();
+			if(thisEnemy == null)
+			{
+				return 0f;
+			}
+		}
+		return Mathf.Abs(Vector2.Distance(transform.localPosition, thisEnemy.transform.localPosition));
 	}
 
 	private List<Enemy> GetEnemiesInRange()
@@ -29,12 +107,11 @@ public class Tower : MonoBehaviour {
 		List<Enemy> enemiesInRange = new List<Enemy>();
 		foreach(Enemy enemy in GameManager.Instance.EnemyList)
 		{
-			if(Vector2.Distance(transform.position, enemy.transform.position) <= attackRadius) // distance between two objects
+			if(Vector2.Distance(transform.localPosition, enemy.transform.localPosition) <= attackRadius) // distance between two objects
 			{
 				enemiesInRange.Add(enemy);
 			}
 		}
-
 		return enemiesInRange;
 	}
 
@@ -45,13 +122,12 @@ public class Tower : MonoBehaviour {
 
 		foreach(Enemy enemy in GetEnemiesInRange())
 		{
-			if(Vector2.Distance(transform.position, enemy.transform.position) < smallestDistance)
+			if(Vector2.Distance(transform.localPosition, enemy.transform.localPosition) < smallestDistance)
 			{
-				smallestDistance = Vector2.Distance(transform.position, enemy.transform.position);
+				smallestDistance = Vector2.Distance(transform.localPosition, enemy.transform.localPosition);
 				nearestEnemy = enemy;
 			}
 		}
-
 		return nearestEnemy;
 	}
 }
