5ddeed8cefb6bce73f40dc5c9302f14be866ec00
Ivan Andonov <ivan.andonovv@hotmail.com>
2018-05-29 14:15:53 +0200

    Adding towers with button presses with specified raycasts

--- TowerDefense/Assets/Scripts/TowerManager.cs
+++ TowerDefense/Assets/Scripts/TowerManager.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 public class TowerManager : Singleton<TowerManager> {
 
@@ -13,12 +14,27 @@ public class TowerManager : Singleton<TowerManager> {
 	
 	// Update is called once per frame
 	void Update () {
-		
+		if (Input.GetMouseButtonDown(0))
+		{
+			Vector2 worldPoint = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+			RaycastHit2D hit = Physics2D.Raycast(worldPoint, Vector2.zero);
+
+			if(hit.collider.tag == "buildSite")
+				PlaceTower(hit);
+		}
 	}
 
-	public void SelectedTower(TowerButton towerSelected)
+	public void PlaceTower(RaycastHit2D hit)
+	{
+		if (!EventSystem.current.IsPointerOverGameObject() && towerButtonPressed != null)
+		{
+			GameObject newTower = Instantiate(towerButtonPressed.TowerObject);
+			newTower.transform.position = hit.transform.position;
+		}
+	}
+
+	public void SelectedTower(TowerButton towerSelected) // which tower button is pressed
 	{
 		towerButtonPressed = towerSelected;
-		Debug.Log("Pressed!" + towerButtonPressed.gameObject);
 	}
 }
